77b1ed6d8385a1f155dad15323e7b8c6a2b61cd2
Lev Nemchuk <lev@adjoy.com>
2017-10-06 10:27:27 -0700

    Added: PgApplicationTests.test1 test

---
+++ pg-1/src/test/java/com/adjoy/PgApplicationTests.java
@@ -0,0 +1,24 @@
+package com.adjoy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PgApplicationTests
+{
+
+	@Test
+	public void contextLoads()
+	{
+	}
+
+	@Test
+	public void test1()
+	{
+		// add test logic here and remove this comment. Courtesy to Gerry! He does not like comments... But I do!
+	}
+
+}
