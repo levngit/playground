3836724ee4222095d04f64654544762fcb8fa8c8
Lev Nemchuk <lev@adjoy.com>
2017-10-06 11:21:16 -0700

    Modified: Change code style. Move open curly brace to the next line.
    I know it is not Unix style but it allow me to see block of code much easier and IntelliJ show block with vertical blue line. Ha-ha-ha.

---
+++ pg-1/src/main/java/com/adjoy/PgApplication.java
@@ -0,0 +1,13 @@
+package com.adjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgApplication extends SpringApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PgApplication.class, args);
+	}
+}
